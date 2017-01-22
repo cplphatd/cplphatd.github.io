76f6afb4d1d975a942f2bd445039f6768cffed80
David Ryan Alviola <david.alviola@gmail.com>
2017-01-22 14:17:03 -0600

    Added social icons.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by David on 1/22/17.
+ */
+"use strict";
+(function () {
+
+    //Smooth scroll event listener
+    $(document).on("click", "a", function(event) {
+        event.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $($.attr(this, "href")).offset().top
+        }, 500);
+    });
+
+})();
